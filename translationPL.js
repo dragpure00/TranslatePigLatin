04f727e5260daad9a69fe86661778e777a9dec90
Ayaz Araf <ayaz.araf@gmail.com>
2020-10-14 22:59:30 -0400

    Add files via upload

---
+++ translationPL.js
@@ -0,0 +1,15 @@
+function translatePL(str){
+
+const regex=(/^[^aeiou]+/g);
+const find=str.match(regex);
+
+if(find==null){
+str=str.concat('way');
+}
+else
+{
+str=str.replace(regex,'').concat(find).concat('ay');
+	}
+return str;
+     }
+console.log(translatePL("consonant");
