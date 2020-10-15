f0a9b2c9b658b535a71443aaf557a05f3be905d9
Ayaz Araf <ayaz.araf@gmail.com>
2020-10-14 23:00:46 -0400

    Update translationPL.js
    edited in some spaceing

--- translationPL.js
+++ translationPL.js
@@ -3,13 +3,14 @@ function translatePL(str){
 const regex=(/^[^aeiou]+/g);
 const find=str.match(regex);
 
-if(find==null){
-str=str.concat('way');
-}
-else
-{
-str=str.replace(regex,'').concat(find).concat('ay');
-	}
-return str;
+     if(find==null)
+     {
+      str=str.concat('way');
      }
+    else
+     {
+       str=str.replace(regex,'').concat(find).concat('ay');
+     }
+return str;
+  }
 console.log(translatePL("consonant");
